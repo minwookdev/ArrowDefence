2d344dee5658c1c5be86043b914adba18a21f6dd
iay1020 <iay1020@naver.com>
2021-04-29 15:37:01 +0900

    인게임 씬 추가, 플레이어 스프라이트, 플레이어 간단이동 스크립트, 오브젝트 콜라이전 테스트

---
+++ CodeGirlBay_Prototype_01/Assets/2. Scripts/Move/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    GameObject playerGO;
+    Transform playerTR;
+
+    private void Awake()
+    {
+        playerGO = this.gameObject;
+        playerTR = playerGO.transform;
+    }
+
+    private void Update()
+    {
+        PushMoveKey();
+    }
+
+    private void PushMoveKey()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            playerTR.position += Vector3.left * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            playerTR.position += Vector3.right * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            playerTR.position += Vector3.down * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            playerTR.position += Vector3.up * 0.1f;
+        }
+    }
+}
--- CodeGirlBay_Prototype_01/Assets/2. Scripts/Tween/TweenPosition.cs
+++ CodeGirlBay_Prototype_01/Assets/2. Scripts/Tween/TweenPosition.cs
@@ -155,5 +155,4 @@ public class TweenPosition : MonoBehaviour
     {
         staying = false;
     }
-
 }

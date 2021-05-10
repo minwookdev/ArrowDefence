95847e1f7a5b1b857330aff8b7ee00b8c1f39add
minwookdev <crow5162@gmail.com>
2021-05-11 03:04:34 +0900

    2021/05/11 ArrowDefence 작업내역 0. ArrowDefence_BattleScene_01 Scene에서 작업 1. 터치/클릭 각도 받아서 회전하는 활 구현. 2. 화살 발사 구현. 3. ObjectPool Manager 사용한 화살 재 장전 기반 준비.

---
+++ CodeGirlBay_Prototype_01/Assets/2. Scripts/CodingCat_Script/CodingCat_Management/CodingCat_CatLog/AD_GameScripts.cs
@@ -0,0 +1,11 @@
+﻿public static class AD_GameScripts
+{
+    public const string BowTag = "ArrowDefence_Bow";
+
+    public enum ArrowAttrubute
+    {
+        Arrow_Normal  = 0,
+        Arrow_Effect  = 1,
+        Arrow_Special = 2
+    }
+}

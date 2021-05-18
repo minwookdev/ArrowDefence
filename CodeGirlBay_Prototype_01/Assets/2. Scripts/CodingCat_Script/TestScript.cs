c5ce70cdc3c461cf8033fec6b3fd1ae8b185d676
minwookdev <crow5162@gmail.com>
2021-05-19 01:27:49 +0900

    2021/05/18 MinWook :: Commit * 작업내역 1. 임시 Title Scene 제작중 2. DoTween 에셋 사용해보는중
     * 작업계획
    1. Title Scene, Main Scene 작업 후 씬전환 문제없이 되도록 작업예정.
    2. MainScene 인벤토리 제작 예정.
    3. Player Data (Invnetory, Play Data, 결제 데이터, 업적 등등) 저장 및 불러오기
     -> Player의 Google Drive에 저장 또는 Local 장치에 저장하도록 예정

---
+++ CodeGirlBay_Prototype_01/Assets/2. Scripts/CodingCat_Script/TestScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void OnMouseDrag()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePosition;
+    }
+}

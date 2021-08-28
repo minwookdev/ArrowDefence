876a4c182c38c9def3ce5370a613c79bd8f61432
minwookdev <crow5162@gmail.com>
2021-08-28 20:55:40 +0900

    <feature>:<Player Data Editor Window 기능 추가>
    <description>
    1. Data Window 에서 플레이어의 인벤토리 내부의 아이템을 시각적으로 볼 수 있도록 기능을 추가.
    2. 현재 장착중인 아이템 Reference 매치가 안되는 원인 파악중.
    3. AD_Inventory 자체의 저장 스크립트 추가 전 Backup
    
    <issue-number>
    1. Inventory Item 불러온 후 Equipment Item과 Reference 매치가 제대로 되지 않고있다.
    2. 불러온 아이템들 정보 제대로 남아있는지 확인.

--- CodeGirlBay_Prototype_01/Assets/2. Scripts/CodingCat_Script/CodingCat_Editor/PlayerData_Editor.cs
+++ CodeGirlBay_Prototype_01/Assets/2. Scripts/CodingCat_Script/CodingCat_Editor/PlayerData_Editor.cs
@@ -1,10 +1,29 @@
-﻿using CodingCat_Games.Data;
+﻿using System.Collections.Generic;
+using CodingCat_Games;
+using CodingCat_Games.Data;
+using CodingCat_Scripts;
 using UnityEditor;
 using UnityEngine;
 
 public class PlayerData_Editor : EditorWindow
 {
-    //private int playerInt;
+    SerializedObject so;
+    public List<ItemInfo> invenInfo = new List<ItemInfo>();
+    Vector2 scrollPos; //스크롤 바의 위치
+
+    public void SetItemInfo(List<AD_item> itemList)
+    {
+        this.invenInfo.Clear();
+    
+        if (itemList != null)
+        {
+            for (int i = 0; i < itemList.Count; i++)
+            {
+                invenInfo.Add(new ItemInfo(itemList[i]));
+            }
+        }
+        else CatLog.Log("Item Info is null, return this Function");
+    }
 
     //Add Menu item named "PlayerData_Editor" to the Window menu
     [MenuItem("CodingCat/Data Window")]
@@ -13,18 +32,43 @@ public class PlayerData_Editor : EditorWindow
         EditorWindow.GetWindow(typeof(PlayerData_Editor));
     }
 
+    private void OnEnable()
+    {
+        //scriptable = new ScriptableObject(CCPlayerData.inventory);
+        //inven = new SerializedObject(CCPlayerData.inventory);
+        //itemList = CCPlayerData.inventory.GetItemList();
+        //
+        //ScriptableObject target = this;
+        //so = new SerializedObject(target);
+    }
+
     private void OnGUI()
     {
+        SetItemInfo(CCPlayerData.inventory.GetAllItemList());
+
         GUILayout.Label("PlayerData Window", EditorStyles.boldLabel);
 
         GUILayout.BeginVertical("HelpBox");
 
         GUILayout.Label("ArrowDefenece PlayerData");
 
-        EditorGUILayout.LabelField("Player int Value : ", CCPlayerData.user_int.ToString());
+        //EditorGUILayout.LabelField("Player int Value : ", CCPlayerData.user_int.ToString());
+        //
+        //GUILayout.Space(10);
+
+        EditorGUILayout.LabelField("Total inventory Items : ", CCPlayerData.inventory.GetAllItemList().Count.ToString());
+
+        ScriptableObject target = this;
+        SerializedObject so = new SerializedObject(target);
+        SerializedProperty itemList = so.FindProperty("invenInfo");
+
+        scrollPos = GUI.BeginScrollView(new Rect(0, 60, 450, 600), scrollPos, new Rect(0, 60, 400, 1500));
+
+        EditorGUILayout.PropertyField(itemList, true);
+
+        GUI.EndScrollView();
 
         GUILayout.Space(100);
-
 
         GUILayout.FlexibleSpace();  //화면 하단에 배치
         GUILayout.EndVertical();
@@ -33,9 +77,45 @@ public class PlayerData_Editor : EditorWindow
 
     }
 
-    //private void Update()
+    //요거 키면 Window 열고있을때 바로바로 업데이트됨 근데 에디터 많이 느려질 수도 있음
+    //특정 조건에서만 켜던지 하자
+    //private void OnInspectorUpdate()
     //{
-    //    
+    //    Repaint();
     //}
+}
+
+[System.Serializable]
+public class ItemInfo
+{
+    public string Id;
+    public string Name;
+    public string Desc;
+    public string Grade;
+    public string Amount;
+    public string Type;
+    public string EquipType;
+    public Sprite ItemSprite;
+
+    public ItemInfo(AD_item item)
+    {
+        this.Id         = item.GetID.ToString();
+        this.Name       = item.GetName;
+        this.Desc       = item.GetDesc;
+        this.Grade      = item.GetGrade.ToString();
+        this.Amount     = item.GetAmount.ToString();
+        this.Type       = item.GetItemType.ToString();
+        this.ItemSprite = item.GetSprite;
+
+        //switch (item)
+        //{
+        //    case Item_Material matitem:     break;
+        //    case Item_Consumable conitem:   break;
+        //    case Item_Bow bowitem:          break;
+        //    case Item_Arrow arrowitem:      break;
+        //    case Item_Accessory accessitem: break;
+        //    default: break;
+        //}
+    }
 }
 

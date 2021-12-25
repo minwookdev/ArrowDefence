47137021f3d0b4bf4fafd0746f74a602111e93d7
minwookdev <crow5162@gmail.com>
2021-12-25 23:53:20 +0900

    <Feature>:<Floating Damage System>
    <description>
     1. 데미지 수치 가시화를 위한 플로팅 데미지 시스템 구현 중.
     3. 오버헤드 체크 및 최적화 진행 예정.
    
     4. Stage Challenge mission을 구현하고 이를 활용한 Auto-mode 구현예정.
    
    <issue-number>
    issue number 0. Item Drop Rate및 Table 개선예정.

---
+++ ArrowDefence_Project/Assets/2. Scripts/CodingCat_Script/00.Battle Script/FloatingDamage.cs
@@ -0,0 +1,68 @@
+﻿namespace ActionCat {
+    using UnityEngine;
+    using UnityEngine.UI;
+    using TMPro;
+
+    public class FloatingDamage : MonoBehaviour {
+        [Header("COMPONENT")] //Caching This Properties.
+        [SerializeField] Transform floatingTr     = null;
+        [SerializeField] TextMeshProUGUI tmpCount = null;
+        [SerializeField] Image imageCrit          = null;
+        [SerializeField] CanvasGroup canvasGroup  = null;
+
+        [Header("PROPERTY")]
+        public float MoveDuration = 1f;
+        public float MoveDistance = 1f;
+        public float FadeDuration = 0.5f;
+        
+        //private variables
+        bool isCritical = false;
+
+        void Update() {
+            if(Input.GetKeyDown(KeyCode.A)) {
+                OnFloating("30", new Vector2(0f, 1f));
+            }
+        }
+
+        public void OnFloating(string countstring, Vector2 direction) {
+            StartCoroutine(FloatingProgress(countstring, direction, isReverse:false));
+        }
+
+        System.Collections.IEnumerator FloatingProgress(string countstring, Vector2 direction, bool isReverse) {
+            //Phase 0. Init Values
+            float progress = 0f;
+            float speed = 1 / MoveDuration;
+            Vector2 targetPosition = (Vector2)floatingTr.position + (direction.normalized * MoveDistance);
+            ///is Reverse rotation
+            ///Vector2 targetPos = (isCritical) ? (Vector2)tr.position - (direction.normalized * MoveDistance) : (Vector2)tr.position + (direction.normalized * MoveDistance);
+            canvasGroup.alpha = 1f;
+            tmpCount.text = countstring;
+
+            //Phase 1. Move to Target Position.
+            while(progress < 1) {
+                progress += Time.deltaTime * speed;
+                floatingTr.position = Vector3.Lerp(floatingTr.position, targetPosition, progress);
+                yield return null;
+            }
+            //Check. Position
+            if ((Vector2)floatingTr.position != targetPosition)
+                floatingTr.position = targetPosition;
+
+            //Phase 2. Disable alpha.
+            progress = 0f;
+            speed = 1 / FadeDuration;
+            while (progress < 1) {
+                progress += Time.deltaTime * speed;
+                canvasGroup.alpha = Mathf.Lerp(1f, 0f, progress);
+                yield return null;
+            }
+            //Check. Alpha
+            if (canvasGroup.alpha != 0f)
+                canvasGroup.alpha = 0f;
+            tmpCount.text = "";
+
+            //Disable GameObejct (this action is stop the coroutine)
+            gameObject.SetActive(false);
+        }
+    }
+}
